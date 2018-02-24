c2f7fe1b64aa31f381bcb6142c8a0025c8be2f25
Animesh <animeshpandey070@gmail.com>
2018-02-24 17:30:52 +0530

    File Read Commit

--- src/main/java/FRead.java
+++ src/main/java/FRead.java
@@ -1,13 +1,22 @@
 import java.io.*;
+
 public class FRead {
 
 	public static void main(String[] args) {
-		String s=null;
+		String s[][] = new String[2][];
 		try {
-			File file=new File("./src/main/resources/ipl.csv");
-			BufferedReader br=new BufferedReader(new FileReader(file));
-			while((s=br.readLine())!=null)
-				System.out.println(s);
+			File file = new File("./src/main/resources/ipl.csv");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			s[0] = br.readLine().split(",");
+			s[1] = br.readLine().split(",");
+			for (int i = 0; i < s[1].length; i++) {
+				try {
+					int temp = Integer.parseInt(s[1][i]);
+					System.out.println(s[0][i] + " :Integer");
+				} catch (Exception e) {
+					System.out.println(s[0][i] + " :String");
+				}
+			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
